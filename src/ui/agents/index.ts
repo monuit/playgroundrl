d2f11c514286c281d0d7272ea8cfb0e27eebcbfd
Mo Abdulhussain <abdulhu6@gmail.com>
2025-10-16 20:47:28 -0400

    feat: export agent components

---
+++ src/ui/agents/index.ts
@@ -0,0 +1,5 @@
+export { BunnyAgentComponent } from './BunnyAgent';
+export { DroneAgentComponent } from './DroneAgent';
+export { FishAgentComponent } from './FishAgent';
+export { BotAgentComponent } from './BotAgent';
+export { PlowAgentComponent } from './PlowAgent';

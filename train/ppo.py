36a5a29ef8d914f5cb94ddbb5ae7c42eb5ddfbcb
Mo Abdulhussain <abdulhu6@gmail.com>
2025-10-20 13:59:11 -0400

    fix: update environment imports and handle missing gymnasium module errors

--- train/ppo.py
+++ train/ppo.py
@@ -34,7 +34,7 @@ class Args:
     """if toggled, this experiment will be tracked with Weights and Biases"""
     wandb_project_name: str = "LevelTwo3"
     """the wandb's project name"""
-    wandb_entity: str = 'noahsolomon'
+    wandb_entity: str = 'monuit'
     """the entity (team) of wandb's project"""
     capture_video: bool = False
     """whether to capture videos of the agent performances (check out `videos` folder)"""
